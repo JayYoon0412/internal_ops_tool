a819df917e6421443f58f680a35a3f218197cabf
제이_윤지원 <jay.59@jei-yunjiwon-ui-MacBookPro.local>
2023-07-12 21:28:47 +0900

    Improved Time and Memory Efficiency

--- dist/app.service.d.ts
+++
@@ -1,4 +0,0 @@
-export declare class AppService {
-    scrape(filePath: string): Promise<void>;
-    downloadCsvFile(response: any): Promise<void>;
-}
--- src/app.service.ts
+++ src/app.service.ts
@@ -7,49 +7,80 @@ import { createObjectCsvWriter } from 'csv-writer';
 @Injectable()
 export class AppService {
   async scrape(filePath: string) {
-    
     const csvWriter = createObjectCsvWriter({
       path: 'followers_data.csv',
-      header: [
-        { id: 'link', title: '인스타 링크' },
-        { id: 'followerContent', title: '팔로워 수' },
-        { id: 'linkContent', title: '헤더 링크' },
+      header: [ 
+        { id: 'number', title: 'no' },
+        { id: 'name', title: 'store_name' },
+        { id: 'storeId', title: 'store_ID' },
+        { id: 'link', title: 'sns_url' },
+        { id: 'followerContent', title: 'followers' }
       ],
     });
 
+    const rows: any[] = [];
+    let rowIndex = 0;
+
     fs.createReadStream(filePath)
       .pipe(csvParser())
-      .on('data', async (row) => {
-        const link = row.instalink;
-        if (link.indexOf('instagram.com')==-1) {
-          //인스타그램 링크가 아닐시 무시
-        } else {
-        const browser = await puppeteer.launch({ headless: false });
-        const page = await browser.newPage();
-        await page.setViewport({ width: 1280, height: 720 });
-        await page.goto(link);
-        await page.waitForTimeout(6000);
-
-        const followersCount = await page.$x('//span[@class="_ac2a"]/@title')
-        let followerNum = await page.evaluate((element) => element.textContent, followersCount[0]);
-        
-        //불러온 데이터 프로세싱: 숫자 형태로 모두 변환
-        let followerContent : string | number;
-        if (followerNum.indexOf('만')!=-1) {
-          followerNum = followerNum.slice(0, -1);
-          followerContent = parseFloat(followerNum) * 10000;
-        } else {
-          followerContent = followerNum
-        }
-        
-        const profileLink = await page.$x('//span[@class="x1lliihq x193iq5w x6ikm8r x10wlt62 xlyipyv xuxw1ft"]')
-        const linkContent = profileLink[0] ? await page.evaluate((element) => element.textContent, profileLink[0]) : '없음';
-
-        await csvWriter.writeRecords([{ link, followerContent, linkContent }]);
-        await browser.close();
-      }
+      .on('data', (row) => {
+        rows.push(row);
       })
       .on('end', async () => {
+        const chunkSize = 30;
+        const totalRows = rows.length;
+
+        for (let startIndex = 0; startIndex < totalRows; startIndex += chunkSize) {
+          const endIndex = Math.min(startIndex + chunkSize, totalRows);
+          const chunkRows = rows.slice(startIndex, endIndex);
+
+          const browser = await puppeteer.launch({ headless: false });
+
+          const promises = chunkRows.map(async (row) => {
+            const link = row.instalink;
+            const number = row.number;
+            const name = row.store_name;
+            const storeId = row.store_ID;
+
+            if (link.indexOf('instagram.com') === -1) {
+              // 경우 1: SNS가 인스타그램이 아닐 경우
+              let followerContent = ''
+              await csvWriter.writeRecords([{ number, name, storeId, link, followerContent }]);
+              return;
+            }
+
+            try {
+              const page = await browser.newPage();
+              await page.setViewport({ width: 1280, height: 720 });
+              await page.goto(link);
+              await page.waitForTimeout(10000);
+
+              const followersCount = await page.$x('//span[@class="_ac2a"]/@title');
+              let followerNum = await page.evaluate((element) => element.textContent, followersCount[0]);
+
+              // 만 단위 숫자로 변환
+              let followerContent: string | number;
+              if (followerNum.indexOf('만') !== -1) {
+                followerNum = followerNum.slice(0, -1);
+                followerContent = parseFloat(followerNum) * 10000;
+              } else {
+                followerContent = followerNum;
+              }
+
+              await csvWriter.writeRecords([{ number, name, storeId, link, followerContent }]);
+            } catch (error) {
+              // 경우 2: 잘못된 인스타그램 링크일 경우
+              console.log('WARNING: invalid link');
+              let followerContent = 'wrong link'
+              await csvWriter.writeRecords([{ number, name, storeId, link, followerContent }]);
+            }
+          });
+
+          await Promise.all(promises);
+
+          await browser.close();
+        }
+
         console.log('Scraping completed successfully!');
       });
   }
@@ -65,3 +96,5 @@ export class AppService {
     fileReadStream.pipe(response);
   }
 }
+
+
