50ee7222bd5fdfafa7e2b1af3a2309beaff1631c
제이_윤지원 <jay.59@jei-yunjiwon-ui-MacBookPro.local>
2023-07-26 12:43:59 +0900

    Changed User Agent Browser

--- src/app.service.ts
+++ src/app.service.ts
@@ -46,7 +46,7 @@ export class AppService {
 
           const browser = await puppeteer.launch({ 
             headless: true,
-            executablePath: '/usr/bin/chromium-browser'
+            executablePath: '/usr/bin/chromium-browser',
           });
 
           const promises = chunkRows.map(async (row) => {
@@ -63,6 +63,7 @@ export class AppService {
 
             try {
               const page = await browser.newPage();
+              await page.setUserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36");
               await page.setViewport({ width: 1280, height: 720 });
               await page.goto(link);
               const pageTitle = await page.evaluate(() => document.title);
