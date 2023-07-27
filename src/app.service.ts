92a04271026b76845da3fc5d936dcf778ef479c0
제이_윤지원 <jay.59@jei-yunjiwon-ui-MacBookPro.local>
2023-07-27 14:28:55 +0900

    Path of Manually Installed Chromium

--- src/app.service.ts
+++ src/app.service.ts
@@ -45,7 +45,8 @@ export class AppService {
           const chunkRows = rows.slice(startIndex, endIndex);
 
           const browser = await puppeteer.launch({ 
-            headless: true
+            headless: true,
+            executablePath: '/usr/bin/chromium-browser'
           });
 
           
