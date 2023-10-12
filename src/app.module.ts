b6d6a37bf814929678d16425ba7d3ab463f4d9d9
제이_윤지원 <jay.0412@jei-yunjiwon-ui-MacBookPro.local>
2023-10-12 11:47:07 +0900

    Updated Element Tagging

--- src/app.module.ts
+++ src/app.module.ts
@@ -18,6 +18,7 @@ import * as winston from 'winston';
 import { registerPartials } from './commons/partials';
 import { LogGateway } from './apis/log/log.gateway';
 import { DashboardTransport } from './apis/log/log.transport';
+import * as schedule from 'node-schedule';
 
 @Module({
   imports: [
@@ -58,5 +59,22 @@ import { DashboardTransport } from './apis/log/log.transport';
 export class AppModule {
   constructor() {
     registerPartials();
+
+    const rule = new schedule.RecurrenceRule();
+    rule.dayOfWeek = [0, new schedule.Range(2,4)];
+    rule.hour = 17;
+    rule.minute = [5, 6, 7];
+    rule.tz = 'Asia/Seoul';
+    schedule.scheduleJob(rule, function() {
+      const date = Date.now();
+      console.log(`[${date}] 자동작업 테스팅 확인`);
+    })
+    
   }
+
+  //엑셀 파일 업로드하고 버튼 누르면 [상품 Id, 재고수량] 맵으로 가능
+  //만약 키 중 수량이 x보다 크면 태그 부여 [태그 부여된 상품 id 리스트] 생성
+  //기존 리스트 업데이트
+  //파센 작업 API로 대체 => 상품 진열/미진열, 그 외 로직
+  
 }
